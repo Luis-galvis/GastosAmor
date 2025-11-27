2283c1c606a97e5820be46892a6c7f59a77fd74f
Luis Galvis <lgalvismoreno@gmail.com>
2025-11-27 16:25:40 -0500

    Add console logs for Supabase environment variabless
    Log Supabase URL and ANON key to the console.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+console.log("URL:", import.meta.env.VITE_SUPABASE_URL)
+console.log("ANON:", import.meta.env.VITE_SUPABASE_ANON_KEY)
 
 createRoot(document.getElementById("root")!).render(<App />);
